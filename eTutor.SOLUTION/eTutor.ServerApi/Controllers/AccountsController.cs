96be4aebc5d51057e74381ca50ba158eccbd08b1
Juan DAniel Ozuna <juandanielozuna2@gmail.com>
2019-11-05 00:24:24 -0400

    Finalized creating login endpoint

--- eTutor.SOLUTION/eTutor.ServerApi/Controllers/AccountsController.cs
+++ eTutor.SOLUTION/eTutor.ServerApi/Controllers/AccountsController.cs
@@ -28,11 +28,30 @@ namespace eTutor.ServerApi.Controllers
 
         [HttpPost]
         [AllowAnonymous]
-        public async Task<IActionResult> Login(UserLoginRequest request)
+        public async Task<IActionResult> Login([FromBody] UserLoginRequest request)
         {
-            var result = await _signInManager.PasswordSignInAsync(request.Email, request.Password, false, false);
+            try
+            {
+                var result = await _signInManager.PasswordSignInAsync(request.Email, request.Password, false, false);
 
-            return Ok();
+                if (!result.Succeeded)
+                {
+                    return BadRequest("User not found");
+                }
+
+                User user = await _userManager.FindByNameAsync(request.Email);
+                var roles = await _userManager.GetRolesAsync(user);
+
+                return Ok(new
+                {
+                    user = user,
+                    roles = roles
+                });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
         }
 
 
