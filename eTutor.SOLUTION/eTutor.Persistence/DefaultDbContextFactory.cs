c53adaf688d85262956662de6b58b34132ccb56c
Juan Daniel Ozuna <juandanielozuna2@gmail.com>
2019-12-03 23:33:50 -0400

    Changed database

--- eTutor.SOLUTION/eTutor.Persistence/DefaultDbContextFactory.cs
+++ eTutor.SOLUTION/eTutor.Persistence/DefaultDbContextFactory.cs
@@ -25,7 +25,7 @@ namespace eTutor.Persistence
 
         public ETutorContext CreateDbContext(string[] args)
         {
-            return Create(_configuration.GetConnectionString("MainConnection"));
+            return Create(_configuration.GetConnectionString("AzureConnection"));z
         }
 
         ETutorContext Create(string connectionString)
