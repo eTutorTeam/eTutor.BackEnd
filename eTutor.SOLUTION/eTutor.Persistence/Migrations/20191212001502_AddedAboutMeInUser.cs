09d7401224bb678260f51b4f83319e49dd985234
AngelR24 <49293458+AngelR24@users.noreply.github.com>
2019-12-14 12:15:34 -0400

    Added AboutMe

---
+++ eTutor.SOLUTION/eTutor.Persistence/Migrations/20191212001502_AddedAboutMeInUser.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eTutor.Persistence.Migrations
+{
+    public partial class AddedAboutMeInUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AboutMe",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "0947df51-3172-4fc1-afcb-83208374273a");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "18d3a2c3-246f-425c-87b5-906cfd45ed96");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "61c77da0-cdbc-4646-be25-9b23fc6a22c0");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "f889abaa-b5d0-4976-985a-0c4cf637134b");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AboutMe",
+                table: "Users");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "45fd03c8-4381-433c-bc88-7195ea240c3a");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "824fd9f9-acbc-4644-b03e-0ff751a0dfdb");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "42ec3ff1-1457-48d9-a9dd-e66acf19a1de");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "8aea4e01-75a9-4640-9087-2b9bba699458");
+        }
+    }
+}
---
+++ eTutor.SOLUTION/eTutor.Persistence/Migrations/20191212173217_Added AboutMe.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eTutor.Persistence.Migrations
+{
+    public partial class AddedAboutMe : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "0933689e-c706-4759-8014-18cbee8a58b4");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "e2cd38fe-f4a3-42d2-b583-15be13a20fc0");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "699a2ab1-656b-4d11-a695-af0c7ebcad83");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "1c5cdb46-165a-4702-8752-3ad7e29d1e26");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "0947df51-3172-4fc1-afcb-83208374273a");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "18d3a2c3-246f-425c-87b5-906cfd45ed96");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "61c77da0-cdbc-4646-be25-9b23fc6a22c0");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "f889abaa-b5d0-4976-985a-0c4cf637134b");
+        }
+    }
+}
