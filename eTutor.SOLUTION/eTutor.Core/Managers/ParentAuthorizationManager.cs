3371721058de438c74415f2109c9164100374896
Juan DAniel Ozuna <juandanielozuna2@gmail.com>
2019-12-25 21:34:41 -0400

    Added information to notification and refactored actions to function dictionary

--- eTutor.SOLUTION/eTutor.Core/Managers/ParentAuthorizationManager.cs
+++ eTutor.SOLUTION/eTutor.Core/Managers/ParentAuthorizationManager.cs
@@ -14,7 +14,7 @@ namespace eTutor.Core.Managers
         private readonly IMeetingRepository _meetingRepository;
         private readonly INotificationService _notificationService;
         private readonly NotificationManager _notificationManager;
-        private Dictionary<ParentAuthorizationStatus, Func<Meeting, Task>> _notificationActions;
+        private Dictionary<ParentAuthorizationStatus, Func<Meeting, ParentAuthorization, Task>> _notificationActions;
 
         public ParentAuthorizationManager(IParentAuthorizationRepository parentAuthorizationRepository,
             IMeetingRepository meetingRepository, INotificationService notificationService, 
@@ -24,7 +24,7 @@ namespace eTutor.Core.Managers
             _meetingRepository = meetingRepository;
             _notificationService = notificationService;
             _notificationManager = notificationManager;
-            _notificationActions = new Dictionary<ParentAuthorizationStatus, Func<Meeting, Task>>
+            _notificationActions = new Dictionary<ParentAuthorizationStatus, Func<Meeting, ParentAuthorization, Task>>
             {
                 {ParentAuthorizationStatus.Approved, NotifyStuentAndTutorOfApprovedMeeting},
                 {ParentAuthorizationStatus.Rejected, NotifyStudentOfRejectedMeeting}
@@ -59,12 +59,12 @@ namespace eTutor.Core.Managers
 
             await _meetingRepository.Save();
 
-            await _notificationActions[authorization.Status](meeting);
+            await _notificationActions[authorization.Status](meeting, authorization);
             
             return BasicOperationResult<ParentAuthorization>.Ok(authorization);
         }
 
-        private async Task NotifyStuentAndTutorOfApprovedMeeting(Meeting meeting)
+        private async Task NotifyStuentAndTutorOfApprovedMeeting(Meeting meeting, ParentAuthorization authorization)
         {
             var student = meeting.Student;
             var tutor = meeting.Tutor;
@@ -79,15 +79,24 @@ namespace eTutor.Core.Managers
             await _notificationService.SendNotificationToUser(student, studentMessage, title);
         }
 
-        private async Task NotifyStudentOfRejectedMeeting(Meeting meeting)
+        private async Task NotifyStudentOfRejectedMeeting(Meeting meeting, ParentAuthorization authorization)
         {
             var student = meeting.Student;
 
             string message = $"Su tutoría con {meeting.Tutor.FullName} ha sido rechazada por su padre.";
 
+            if (authorization.Reason != null)
+            {
+                message += " " + authorization.Reason;
+            }
+            
+
             string title = "Tutoría Denegada";
 
-            await _notificationService.SendNotificationToUser(student, message, title);
+            await _notificationService.SendNotificationToUser(student, message, title, new Dictionary<string, string>
+            {
+                {"rejectedMeeting", "true"},
+            });
         }
         
     }
