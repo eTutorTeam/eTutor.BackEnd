2807132d27eb2be98d87e61ef1fd38b2f48c46dc
Juan DAniel Ozuna <juandanielozuna2@gmail.com>
2020-01-05 19:20:43 -0400

    Added migration for ended meeting time

---
+++ eTutor.SOLUTION/eTutor.Persistence/Migrations/20200105230948_CreateNewFieldToAddRealEndTime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eTutor.Persistence.Migrations
+{
+    public partial class CreateNewFieldToAddRealEndTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RealEndedDateTime",
+                table: "Meetings",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.DropColumn(
+                name: "RealEndedDateTime",
+                table: "Meetings");
+        }
+    }
+}
